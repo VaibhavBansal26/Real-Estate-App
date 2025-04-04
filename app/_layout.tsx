75e58adddc7dd61d602b70ae2bd03785a48ed4a3
VaibhavBansal26 <vaibhav.bansal945@gmail.com>
2025-04-04 01:13:44 -0400

    app completion

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,34 @@
-import { SplashScreen, Stack } from "expo-router";
-
-import "./global.css"
-
-import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
+import "./global.css";
+import GlobalProvider from "@/lib/global-provider";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
-    "Rubik-Bold": require('../assets/fonts/Rubik-Bold.ttf'),
-    "Rubik-ExtraBold": require('../assets/fonts/Rubik-ExtraBold.ttf'),
-    "Rubik-Light": require('../assets/fonts/Rubik-Light.ttf'),
-    "Rubik-Medium": require('../assets/fonts/Rubik-Medium.ttf'),
-    "Rubik-Regular": require('../assets/fonts/Rubik-Regular.ttf'),
-    "Rubik-SemiBold": require('../assets/fonts/Rubik-SemiBold.ttf')
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
   });
 
   useEffect(() => {
-    if (!fontsLoaded) {
+    if (fontsLoaded) {
       SplashScreen.hideAsync();
-    } else {
-      console.log("Fonts are loaded");
     }
   }, [fontsLoaded]);
-  // Hide the splash screen once the fonts are loaded
+
   if (!fontsLoaded) {
-    return null
+    return null;
   }
-  return <Stack  screenOptions={{headerShown:false}}/>;
+
+  return (
+    <GlobalProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </GlobalProvider>
+  );
 }
--- lib/seed.ts
+++ lib/seed.ts
@@ -16,24 +16,24 @@ const COLLECTIONS = {
 
 const propertyTypes = [
   "House",
-  "Townhomes",
-  "Condos",
-  "Duplexes",
-  "Studios",
+  "Townhouse",
+  "Condo",
+  "Duplex",
+  "Studio",
   "Villa",
-  "Apartments",
-  "Others",
+  "Apartment",
+  "Other",
 ];
 
 const facilities = [
   "Laundry",
-  "Car Parking",
-  "Sports Center",
+  "Parking",
+  "Sports-Center",
   "Cutlery",
   "Gym",
-  "Swimming pool",
+  "Swimming",
   "Wifi",
-  "Pet Center",
+  "Pet-friendly",
 ];
 
 function getRandomSubset<T>(
